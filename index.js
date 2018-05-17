87a0fd242396c0bb58c1b1d03210bab33b3a7458
Edward D'Amato <info@edwarddamato.com>
2018-05-17 12:02:23 +0100

    More stuff

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-var component = require('./src/components/my-component/index');
+// var component = require('./src/components/my-component/index');
+
+const compiler = require('marko/compiler');
+
+const compiledSrc = compiler.compileFile('./src/components/my-component/index.marko');
+console.log(compiledSrc);
